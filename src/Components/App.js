949d23cbe2f7e380358bc580332c683b81558759
Songift <69225621+Songift@users.noreply.github.com>
2020-09-17 07:21:41 +0900

    make Router & file hierarchy

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./Router";
+
+class App extends React.Component {
+  return() {
+    return (
+      <>
+        <Router />
+      </>
+    )
+  }
+}
+
+export default App; 
