4a36ad8291e19cb7c5b7ed461ed8fe35ef3a6dd8
Songift <69225621+Songift@users.noreply.github.com>
2020-09-18 11:20:17 +0900

    Updates

---
+++ precache-manifest.455792814a86ca244551aa8fe11015e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe6489cd306326f3bfca5c041a1030e",
+    "url": "/Songflix/index.html"
+  },
+  {
+    "revision": "f7dd2086cc50ec5fd737",
+    "url": "/Songflix/static/js/2.58f82731.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Songflix/static/js/2.58f82731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f101e31a89aece9359c2",
+    "url": "/Songflix/static/js/main.1b17be16.chunk.js"
+  },
+  {
+    "revision": "3ef21570d816630f272b",
+    "url": "/Songflix/static/js/runtime-main.4594f4b5.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/Songflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
